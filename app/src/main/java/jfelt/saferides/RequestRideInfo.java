9758ca2d939d7c57e43651b0d23ddeda728556e2
Marroque <xexaxmx@gmail.com>
2016-04-29 14:26:16 -0700

    Added GPS Capability
    Bugs: If the button is pressed before the GPS is pulled it will popup
    the message saying GPS is off until the location is received.

--- app/src/main/java/jfelt/saferides/RequestRideInfo.java
+++ app/src/main/java/jfelt/saferides/RequestRideInfo.java
@@ -1,10 +1,19 @@
 package jfelt.saferides;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RequestRideInfo extends AppCompatActivity {
     public final static String EXTRA_NAME = "jfelt.saferides.NAME";
@@ -15,7 +24,9 @@ public class RequestRideInfo extends AppCompatActivity {
     public final static String EXTRA_PICKWITHIN = "jfelt.saferides.PICKWITHIN";
     public final static String EXTRA_DROPWITHIN = "jfelt.saferides.DROPWITHIN";
     public final static String EXTRA_GROUPSIZE = "jfelt.saferides.GROUPSIZE";
-
+    AppLocationService appLocationService;
+    Button btnGetLocation;
+    EditText pickup_address;
     RequestRideConfirm.Client client = new RequestRideConfirm.Client();
 
 
@@ -24,12 +35,35 @@ public class RequestRideInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_ride_info);
+        pickup_address = (EditText) findViewById(R.id.pickup_address);
+        appLocationService = new AppLocationService(
+                RequestRideInfo.this);
 
         Intent intent = getIntent();
         //client.isStudent = intent.getBooleanExtra(RequestRide.EXTRA_ISSTUDENT, false);
         //lient.pickWithin10 = intent.getBooleanExtra(RequestRide.EXTRA_PICKWITHIN, false);
         //client.dropWithin10 = intent.getBooleanExtra(RequestRide.EXTRA_DROPWITHIN, false);
         //client.groupSize = intent.getIntExtra(RequestRide.EXTRA_ISSTUDENT, 1);
+        btnGetLocation = (Button) findViewById(R.id.btnGetLocation);
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Location location = appLocationService
+                        .getLocation(LocationManager.NETWORK_PROVIDER);
+
+                if (location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    LocationAddress locationAddress = new LocationAddress();
+                    locationAddress.getAddressFromLocation(latitude, longitude,
+                            getApplicationContext(), new GeocoderHandler());
+                } else {
+                    showSettingsAlert();
+                }
+
+            }
+        });
 
     }
 
@@ -59,5 +93,43 @@ public class RequestRideInfo extends AppCompatActivity {
         intent.putExtra(EXTRA_GROUPSIZE, client.groupSize);
 
         startActivity(intent);
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                RequestRideInfo.this);
+        alertDialog.setTitle("SETTINGS");
+        alertDialog.setMessage("GPS is disabled! Please enable it in your settings to use this feature.");
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog.setPositiveButton("Settings",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        RequestRideInfo.this.startActivity(intent);
+                    }
+                });
+        alertDialog.show();
+    }
+
+    private class GeocoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String locationAddress;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    locationAddress = bundle.getString("address");
+                    break;
+                default:
+                    locationAddress = null;
+            }
+            pickup_address.setText(locationAddress);
+        }
     }
 }
